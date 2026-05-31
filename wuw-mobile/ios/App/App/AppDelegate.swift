2175a8369d7ddf4b7f5039febf9493ae738db574
Ethan Mamane - mamane.tech <61732869+ethan-mne@users.noreply.github.com>
2026-05-31 18:30:40 +0300

    Fix type in AppDelegate for user activity restoration handler
    - Updated the type of the restorationHandler parameter in the application(_:continue:restorationHandler:) method from [UIActivityRestoring]? to [UIUserActivityRestoring]? for improved type safety and clarity.

--- wuw-mobile/ios/App/App/AppDelegate.swift
+++ wuw-mobile/ios/App/App/AppDelegate.swift
@@ -33,7 +33,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
     }
 
-    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIActivityRestoring]?) -> Void) -> Bool {
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
         return ApplicationDelegateProxy.shared.application(application, continue: userActivity, restorationHandler: restorationHandler)
     }
 
